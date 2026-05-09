7ff223a95c66061f37c34e75dd4b33f71511fdea
matt <khikmat64168@gmail.com>
2026-05-09 11:15:41 +0900

    feat: learn numbers in in python

---
+++ primitive.py
@@ -0,0 +1,15 @@
+print("================================")
+# in Java , variable is a name of storage location 
+
+# in python , variable is a name of reference 
+
+count = 100
+count_type = type(count)
+print("count:" , count , count_type)
+print(f"the count: {count} and type: {count_type}")
+
+result1 = count.bit_count()#method
+result2 = count.numerator
+print(result1, result2)
+
+
