9c8b4fa4b863daff4ccbd246dca91d8ce1889614
matt <khikmat64168@gmail.com>
2026-05-09 12:27:07 +0900

    fix: learn keywords & default arguments

--- functions.py
+++ functions.py
@@ -15,7 +15,7 @@ print("===============   DEFINE vs CALL  =================")
 # Instead of block { } in JAVA  , Python uses indentation to define ({} ning o'rniga)
 
 
-# DEFINE - build /qurish
+# DEFINE - build /qurish. parametr
 
 def greet(a):
     # pass # bu yerda hech narsa bajarilmaydi , lekin sintaksis jihatdan qo'yish kerak
@@ -27,10 +27,31 @@ def greeting(b):
     return f"Hi, {b}"
 
 
-# CALL /execute - chaqirish / bajarish
+# CALL /execute - chaqirish / bajarish.  argument
 result1 = greet('Matt')
 print("result1:", result1)
 
 
 result2 = greeting("John")
 print("result2:", result2)
+
+
+print("===============   Keyword & default arguments  =================")
+# oqilishgina oson bo'lishi uchun
+# DEFINE
+
+
+def give_greet(name, age=30):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old!"
+
+
+# CALL
+
+
+result3 = give_greet(name="Matthew", age=30)
+print("result3:", result3)
+
+
+result4 = give_greet("Jack")
+print("result4:", result4)
