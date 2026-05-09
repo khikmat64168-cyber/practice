caf57c11787a330de60ca65ea875b12d8512b78b
matt <khikmat64168@gmail.com>
2026-05-09 11:01:22 +0900

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
-print("Hello world!")
+#print("Hello world!")
+
+### web fullstack ai engineering , automation/testing, desktop apps ,hacking da kopishlatilinaadii 
+# 
+# 
+# Python 2  va python3 versiyalarri mavjud ####
+
+
+#Dunyodagi barcha tillar COMPILED va INTERPRETED tillarga bo'linadi. Python esa INTERPRETED. Ularning farqii shundaki, COMPILED tillar kodni mashina tiliga o'zgartiradi va keyin bajaradi, INTERPRETED tillar esa kodni to'g'ridan-to'g'ri bajaradi. Pythonning INTERPRETED bo'lishi uni o'qish va tushunishni osonlashtiradi, lekin ba'zan COMPILED tillarga nisbatan sekinroq ishlashi mumkin.
+
+
+
+
+#Dunder (Double Underscore) - bu Python'da maxsus metodlar yoki atributlar uchun (pythonning ichki qurilish mexanizimlari )ishlatiladigan nomlash konventsiyasi.   __builtins__ , __init__
+#print(message)
+
+#result = type(message)
+#print("result:" , result )
+ 
+ 
+'''In python there are built in tools :
+(1)TYPES >  int ,float ,str,list, dict
+(2)FUNCTION  > print() , len() , input(), type() , str() , int()
+(3)CONSTANTS > True , False , None
+'''
+
+print(dir(__builtins__))
+ 
