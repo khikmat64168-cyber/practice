c489152d0df511919f3fbc0cd34dbcf40f7fc1a8
matt <khikmat64168@gmail.com>
2026-05-16 12:01:08 +0900

    feat: learn tuple and list

---
+++ tuples.py
@@ -0,0 +1,40 @@
+'''Tuple
+   (1)What is tuple: typle vs list
+   (2)Unpacking arguments 
+   (3)zip
+   
+
+
+'''
+
+
+print("------ What is tuple: typle vs list ----- ")
+# Java/pHP/ NodeJs array = > Python list
+
+# literal
+
+numbs = [3, 5, 1, 2]
+print(numbs)
+# car_dict = {"brand": "Ferrari", "year": 1995}
+
+# constructor
+
+letters = list("Hello World")
+# person_dict = dict(name="Matt", age=35)
+print(letters)
+
+
+fruits = ["apple", " lemon", "kiwi", "banana"]
+
+print("before fruits:", fruits)
+
+
+fruits[2] = "melon"
+print("after fruits :", fruits)
+
+# tuple
+animals = ("dog", "cat", "fish", "lion ")
+tuple_obj = ("MIT", 100, True, none)
+
+print(animal(0))
+# animals[0] = "bird"
